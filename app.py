36aa42db7376cb42e7383c9cbe1d63b34ebf44ea
Davide Liu <davide97ls@gmail.com>
2024-04-01 15:41:27 +0800

    polish code
    - polish code app.py
    - move index.html in templates

--- app.py
+++ app.py
@@ -1,138 +1,118 @@
-from fastapi import FastAPI,  Request
-from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
-from datetime import datetime
-from database import connect_db
-from fastapi import HTTPException
-from datetime import datetime, timedelta
-import random
-from bson import ObjectId
+import logging
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import json
+from bson import ObjectId
 import uvicorn
+from typing import Optional
+from pydantic import BaseModel, Field
+from datetime import date
+
+from database import connect_db
 
 app = FastAPI()
+app.title = "Tasks Manager"
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
-db_name = 'hw'
-collection = 'tasks'
+DB_NAME = 'hw'
+COLLECTION = 'tasks'
 
-def preprocess_task(task: dict):
+logging.basicConfig(level=logging.INFO)
+
+
+class TaskModel(BaseModel):
+    title: str = Field(..., min_length=1, max_length=30)
+    author: Optional[str] = None
+    description: Optional[str] = None
+    deadline: str = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "My Task",
+                "author": "Davide",
+                "description": "This is a sample task",
+                "deadline": "2022-12-31"
+            }
+        }
+
+
+def preprocess_task(task: dict) -> dict:
     task['id'] = str(task['_id'])
     del task['_id']
     return task
 
-# Endpoints for CRUD operations
+
+def get_db_client() -> object:
+    return connect_db()[DB_NAME]
+
+
+def get_tasks_collection(db_client) -> object:
+    return db_client.get_collection(COLLECTION)
+
+
 @app.post("/create_task")
-async def create_task(request: Request):
-    data = await request.json()
-    try:
-        client = connect_db()
-        db = client[db_name]  # Replace with your database name
-        tasks_collection = db.get_collection(collection)
+async def create_task(request: Request, task: TaskModel) -> dict:
+    tasks_collection = get_tasks_collection(get_db_client())
+    task_data = task.dict()
 
-        task_data = {
-            "title": data['title'],
-            "author": data['author'],
-            "description": data['description'],
-            "deadline": data['deadline'],
-        }
-        print(f'Creating task {task_data}')
+    result = tasks_collection.insert_one(task_data)
+    if not result.inserted_id:
+        raise HTTPException(status_code=400, detail="Failed to create task")
 
-        # Insert the task into the collection
-        result = tasks_collection.insert_one(task_data)
-        if not result.inserted_id:
-            raise HTTPException(status_code=400, detail="Failed to create task")
+    created_task = preprocess_task(tasks_collection.find_one(result.inserted_id))
+    logging.info(f'Task successfully created {created_task}')
+    return created_task
 
-        created_task = tasks_collection.find_one(result.inserted_id)  # Convert to string before using in query
-        created_task = preprocess_task(created_task)
-        print(f'Task successfully created (ID: {result.inserted_id}): {created_task}')
-        return created_task
-
-    except Exception as e:
-        print(f"Error creating task: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
 
 @app.get("/tasks")
-async def get_all_tasks():
-    try:
-        client = connect_db()
-        db = client[db_name]
-        tasks_collection = db.get_collection(collection)
+async def get_all_tasks() -> list:
+    tasks_collection = get_tasks_collection(get_db_client())
+    tasks_list = [preprocess_task(task) for task in tasks_collection.find()]
+    logging.debug(f'Retrieved tasks: {tasks_list}')
+    return tasks_list
 
-        # Find all tasks in the collection
-        tasks_list = []
-        tasks = tasks_collection.find()
-        for task in tasks:
-            task = preprocess_task(task)
-            tasks_list.append(task)
-
-        return tasks_list
-
-    except Exception as e:
-        print(f"Error retrieving tasks: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
 
 @app.get("/tasks/{task_id}")
-async def get_task_by_id(task_id: str, request: Request):
-    try:
-        client = connect_db()
-        db = client[db_name]  # Replace with your database name
-        tasks_collection = db.get_collection(collection)
+async def get_task_by_id(task_id: str, request: Request) -> dict:
+    tasks_collection = get_tasks_collection(get_db_client())
+    task = tasks_collection.find_one({"_id": ObjectId(task_id)})
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task (ID: {task_id}) not found")
 
-        object_id = ObjectId(task_id)
-        task = tasks_collection.find_one({"_id": object_id})
-        if not task:
-            raise HTTPException(status_code=404, detail=f"Task (ID: {task_id}) not found")
+    task = preprocess_task(task)
+    logging.info(f'Task successfully retrieved {task}')
+    return templates.TemplateResponse(
+        request=request, name="task-details.html", context={"request": request, "task": task}
+    )
 
-        print(f'Task successfully retrieved (ID: {task_id}): {task}')
-
-        task = preprocess_task(task)
-
-        return templates.TemplateResponse(
-            request=request, name="task-details.html", context={"request": request,
-                                                                "task": task}
-        )
-
-    except Exception as e:
-        print(f"Error retrieving task: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
 
 @app.put("/tasks/{task_id}")
-async def update_task(task_id: str, updated_data: dict = None):
-    print(task_id, updated_data)
-    try:
-        client = connect_db()
-        db = client[db_name]  # Replace with your database name
-        tasks_collection = db.get_collection(collection)
+async def update_task(task_id: str) -> dict:
+    tasks_collection = get_tasks_collection(get_db_client())
 
-        object_id = ObjectId(task_id)
+    # Find the task to update
+    task = tasks_collection.find_one({"_id": ObjectId(task_id)})
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
 
-        # Find the task to update
-        task = tasks_collection.find_one({"_id": object_id})
-        if not task:
-            raise HTTPException(status_code=404, detail="Task not found")
+    # Update task data
+    task.update(task.dict())
 
-        # Update task data
-        for key, value in updated_data.items():
-            task[key] = value
+    # Update the task in the collection
+    tasks_collection.update_one({"_id": ObjectId(task_id)}, {"$set": task})
 
-        # Update the task in the collection
-        update_result = tasks_collection.update_one({"_id": object_id}, {"$set": task})
+    # Optionally, retrieve the updated task document
+    updated_task = preprocess_task(tasks_collection.find_one({"_id": ObjectId(task_id)}))
+    logging.info(f'Task successfully updated {updated_task}')
+    return updated_task
 
-        # Optionally, retrieve the updated task document
-        updated_task = tasks_collection.find_one({"_id": object_id})
 
-        updated_task = preprocess_task(updated_task)
-        print(f'Task successfully updated (ID: {task_id}): {updated_task}')
-        return updated_task
-
-    except Exception as e:
-        print(f"Error updating task: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error")
+@app.get("/")
+async def index(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context=context)
 
 
 if __name__ == '__main__':
